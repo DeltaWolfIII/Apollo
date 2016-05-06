d3aedf8255be1ce668df4c6a118462b9b9ba6eef
ΔLΞX <123zezima21@gmail.com>
2016-05-06 08:47:31 -0400

    Create project.py

---
+++ project.py
@@ -0,0 +1,30 @@
+from __future__ import unicode_literals
+import youtube_dl
+
+URL = input("Enter a song name: ")
+URL = URL + " audio"
+quality = input("what quality would like your song to be? (low, medium, or high) ")
+if quality == 'low':
+	bitrate = '96'
+if quality == 'medium':
+	bitrate = '192'
+if quality == 'high':
+	bitrate = '320'
+
+
+print("Downloading song, please wait...")
+
+ytdl_options = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': bitrate,
+    }],
+    'default_search': 'auto',
+    'quiet': 'on'
+}
+with youtube_dl.YoutubeDL(ytdl_options) as ytdl:
+   ytdl.download([URL])
+
+print("Your song has finished downloading.")
