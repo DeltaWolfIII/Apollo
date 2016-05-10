702bd8bddee59f453e24835f492f8eb297c5c203
DeltaWolfIII <123zezima21@gmail.com>
2016-05-10 08:27:59 -0400

    added support for python2

--- euterpe.py
+++ euterpe.py
@@ -1,32 +1,7 @@
-from __future__ import unicode_literals
-import youtubedl
+import sys
+import subprocess
 
-URL = input("Enter a song name or a YouTube playlist URL: ")
-if "youtube.com/" not in URL:
-    URL = URL + "audio"
-
-quality = input("What quality would like your song to be? (low, medium, or high) ")
-if quality == 'low':
-	bitrate = '96'
-if quality == 'medium':
-	bitrate = '192'
-if quality == 'high':
-	bitrate = '320'
-
-
-print("Downloading song, please wait...")
-
-ytdl_options = {
-    'format': 'bestaudio/best',
-    'postprocessors': [{
-        'key': 'FFmpegExtractAudio',
-        'preferredcodec': 'mp3',
-        'preferredquality': bitrate,
-    }],
-    'default_search': 'auto',
-    'quiet': 'on'
-}
-with youtubedl.YoutubeDL(ytdl_options) as ytdl:
-   ytdl.download([URL])
-
-print("Your song has finished downloading.")
+if sys.version_info[0] <= 3:
+	subprocess.Popen("python py2.py", shell=True).communicate()
+if sys.version_info[0] >= 3:
+	subprocess.Popen("python3 py3.py", shell=True).communicate()
