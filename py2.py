4069fb37a954b38089fa14ac909a3fa63fdad4fe
DeltaWolfIII <123zezima21@gmail.com>
2016-05-12 08:25:24 -0400

    Added ASCII art and an error message to both py2 and py3

--- py2.py
+++ py2.py
@@ -1,9 +1,22 @@
 from __future__ import unicode_literals
 import youtubedl
 import sys
+asciiapollo = """\
 
+
+  (                  (   (       
+   )\                 )\  )\      
+((((_)(   `  )    (  ((_)((_) (   
+ )\ _ )\  /(/(    )\  _   _   )\  
+ (_)_\(_)((_)_\  ((_)| | | | ((_) 
+  / _ \  | '_ \)/ _ \| | | |/ _ \ 
+ /_/ \_\ | .__/ \___/|_| |_|\___/ 
+         |_|                                                                                                                                                           
+"""
 if sys.version_info[0] > 2.9:
 	print("Python version higher than 2, please run \"py3.py\"")
+if sys.version_info[0] < 3:
+    sys.exit(0)
 URL = raw_input("Enter a song name or a YouTube playlist URL: ")
 if "youtube.com/" not in URL:
     URL = URL + "audio"
