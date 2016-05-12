48e28a45b24f2968f32100f311fa5fcd09538cd7
DeltaWolfIII <123zezima21@gmail.com>
2016-05-12 08:28:52 -0400

    fixed bug

--- py2.py
+++ py2.py
@@ -15,7 +15,7 @@ asciiapollo = """\
 """
 if sys.version_info[0] > 2.9:
 	print("Python version higher than 2, please run \"py3.py\"")
-if sys.version_info[0] < 3:
+if sys.version_info[0] > 2.9:
     sys.exit(0)
 URL = raw_input("Enter a song name or a YouTube playlist URL: ")
 if "youtube.com/" not in URL:
