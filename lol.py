d7402b20d48c8e28bfe0e0709933e6a43a2b907a
ΔLΞX <123zezima21@gmail.com>
2016-05-19 13:52:50 -0400

    Update lol.py

--- lol.py
+++ lol.py
@@ -1,21 +1,12 @@
-# coding=utf-8
-
-import youtube_dl
-from flask import render_template, flash, redirect, session, url_for, request, g, Markup
-from app import app
-
-@app.route('/')
-def index():
-    return render_template('index.html')
-
-
-@app.route('/userdata/', methods=['POST'])
-def data():
-    url = request.form['url']
-    db = open("db.txt","w")
-    db.write(url)
-    db.close()
-    return "It works!"
-@app.route('/about')
-def about():
-    return render_template('about.html')
+ytdl_options = {
+    'format': 'bestaudio/best',
+    'postprocessors': [{
+        'key': 'FFmpegExtractAudio',
+        'preferredcodec': 'mp3',
+        'preferredquality': '320',
+    }],
+    'default_search': 'auto',
+    'quiet': 'on'
+}
+with youtube_dl.YoutubeDL(ytdl_options) as ytdl:
+   ytdl.download([url]
